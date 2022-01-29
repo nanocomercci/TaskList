fd61a7de0c0ea748d7cb07b9d77dea9d61ca1c2d
Luciano Comercci <nanocomercci@gmail.com>
2022-01-29 00:34:29 -0300

    interfaces

---
+++ src/app/components/Task.ts
@@ -0,0 +1,7 @@
+export interface Task{
+    id?:number; // cuando se crea podria no venir
+    text: string;
+    day: string;
+    reminder:boolean;
+    
+}
---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,27 @@
+import{Task} from "./Task"
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Termnar el Primer Módulo de Angular',
+    day: 'Agosto 5 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Termnar el Primer Módulo de Base de Datos',
+    day: 'Agosto 9 a las 14:00',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Irme al choto',
+    day: 'Agosto 19 a las 19:00',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Comerme una chota',
+    day: 'Septiembre 15 a las 14:00',
+    reminder: false,
+  }
+];
